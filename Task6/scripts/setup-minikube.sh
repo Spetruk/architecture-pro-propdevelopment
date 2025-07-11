ac29518ed5a293a000bf1cb07f256ae46fce0d16
A V <spetruk.developer@gmail.com>
2025-07-11 23:55:41 +0300

    f

--- Task6/scripts/setup-minikube.sh
+++ Task6/scripts/setup-minikube.sh
@@ -1,7 +1,14 @@
 minikube delete
 minikube start
-mkdir -p ~/.minikube/files/etc/kubernetes/
-cp "audit-policy.yaml" ~/.minikube/files/etc/kubernetes/audit-policy.yaml
-
-sudo minikube cp kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+minikube cp ~/etc/kubernetes/audit-policy.yaml /etc/kubernetes/audit-policy.yaml
+#minikube cp ~/var/log/audit.log /var/log/audit.log
+minikube cp kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
 minikube start
+#cp "audit-policy.yaml" /Users/av/etc/kubernetes/audit-policy.yaml
+
+#mkdir -p ~/.minikube/files/etc/kubernetes/
+#cp "audit-policy.yaml" ~/.minikube/files/etc/kubernetes/audit-policy.yaml
+
+
+#minikube cp ~/.minikube/files/etc/kubernetes/audit-policy.yaml /etc/kubernetes/audit-policy.yaml
+#minikube start
